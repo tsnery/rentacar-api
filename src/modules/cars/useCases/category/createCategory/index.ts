44d206cb1683d0f4c5028d98d924d69c8b668972
= <=>
2022-09-21 17:13:56 -0300

    fix: getting repository instance following singleton pattern

--- src/modules/cars/useCases/category/createCategory/index.ts
+++ src/modules/cars/useCases/category/createCategory/index.ts
@@ -2,7 +2,7 @@ import { CategoryRepository } from "../../../repositories/category/category.repo
 import { CreateCategoryController } from "./createCategory.controller";
 import { CreateCategoryUseCase } from "./createCategory.useCase";
 
-const categoryRepository = new CategoryRepository()
+const categoryRepository = CategoryRepository.getInstance()
 
 const createCategoryUseCase = new CreateCategoryUseCase(categoryRepository)
 
