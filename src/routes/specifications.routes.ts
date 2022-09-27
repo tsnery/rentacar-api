03b3364565ea4bdd54b838aa5796b2b3b9ca7ac0
Tainan Nery <zaoldyeck@outlook.com>
2022-09-27 20:07:29 -0300

    refactor: listSpecificationsRoutes using tsyringe

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,16 +1,14 @@
 import { Router } from "express";
 import { CreateSpecificationController } from "../modules/cars/useCases/specification/createSpecification/createSpecification.controller";
-// import { listSpecificationsController } from "../modules/cars/useCases/specification/listSpecifications";
+import { ListSpecificationsController } from "../modules/cars/useCases/specification/listSpecifications/listSpecifications.controller";
 
 const createSpecificationController = new CreateSpecificationController()
+const listSpecificationController = new ListSpecificationsController()
 
 const specificationsRoutes = Router()
 
 specificationsRoutes.post('/', createSpecificationController.handle)
-
-// specificationsRoutes.get('/', (request, response) => {
-//   listSpecificationsController.handle(request, response)
-// })
+specificationsRoutes.get('/', listSpecificationController.handle)
 
 export {
   specificationsRoutes
