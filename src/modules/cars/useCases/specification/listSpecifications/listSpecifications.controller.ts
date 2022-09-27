cfd79b30b6dc6eae4efd2778c0023e1797345b11
Tainan Nery <zaoldyeck@outlook.com>
2022-09-27 20:06:07 -0300

    refactor: listSpecifications using tsyringe

--- src/modules/cars/useCases/specification/listSpecifications/listSpecifications.controller.ts
+++ src/modules/cars/useCases/specification/listSpecifications/listSpecifications.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
+import { container } from 'tsyringe';
 import { ListSpecificationsUseCase } from "./listSpecification.useCase";
 
 export class ListSpecificationsController {
+  async handle(request: Request, response: Response) {
+    const listSpecificationsUseCase = container.resolve(ListSpecificationsUseCase)
 
-  constructor(private listSpecificationsUseCase: ListSpecificationsUseCase) { }
-
-  handle(request: Request, response: Response) {
-    const specifications = this.listSpecificationsUseCase.execute()
+    const specifications = await listSpecificationsUseCase.execute()
 
     response.status(200).json(specifications)
   }
