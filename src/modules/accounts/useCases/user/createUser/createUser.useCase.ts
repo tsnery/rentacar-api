4a205b0a80a82c41e12217e0aa9870064ced7767
Tainan Nery <zaoldyeck@outlook.com>
2022-09-28 15:01:51 -0300

    feat: password hash

--- src/modules/accounts/useCases/user/createUser/createUser.useCase.ts
+++ src/modules/accounts/useCases/user/createUser/createUser.useCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO, IUserRepository } from "../../../repositories/user/user.types";
 
@@ -6,11 +7,14 @@ export class CreateUserUseCase {
   constructor(@inject('UserRepository') private userRepository: IUserRepository) { }
 
   async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+
+    const passwordHash = await hash(password, 8)
+
     return this.userRepository.create({
       driver_license,
       email,
       name,
-      password
+      password: passwordHash
     })
   }
 }
