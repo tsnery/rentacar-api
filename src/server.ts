51839e858b0724d5525a27f70e7fb8e6fb933a0f
Tainan Nery <zaoldyeck@outlook.com>
2022-09-29 00:13:34 -0300

    feat: applying middleware to catch errors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata"
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
 import swaggerUI from 'swagger-ui-express'
 import { router } from './routes'
 import { initializeDatabase } from './database'
@@ -7,6 +8,7 @@ import { initializeDatabase } from './database'
 import './shared/container'
 
 import swaggerSetup from './docs/swagger.json'
+import { AppError } from "./errors/AppError"
 
 initializeDatabase()
 
@@ -19,5 +21,13 @@ app.use(express.json())
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSetup))
 
 app.use(router)
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({ message: error.message })
+  }
+
+  return response.status(500).json({ status: 'Error', message: `Internal server error - ${error.message}` })
+})
 
 app.listen(PORT, () => console.log(`Runnning on http://localhost:${PORT}`)) 
