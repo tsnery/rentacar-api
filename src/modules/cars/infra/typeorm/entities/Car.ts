a7609f756f06bf40e228dc6bfe2dc1caa71e407d
Tainan Nery <zaoldyeck@outlook.com>
2022-10-04 16:55:17 -0300

    feat: car entity in memory

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,20 @@
+import { v4 as uuidV4 } from 'uuid'
+
+export class Car {
+  id?: string
+  name!: string
+  description!: string
+  daily_rate!: number
+  is_available!: boolean
+  license_plate!: string
+  fine_amount!: number
+  brand!: string
+  created_at!: Date
+  updated_at!: Date
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4()
+    }
+  }
+}
