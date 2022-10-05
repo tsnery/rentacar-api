7115977c95c7cca2c5eb02d4ee7f10fa5aa7cba2
Tainan Nery <zaoldyeck@outlook.com>
2022-10-05 14:46:06 -0300

    feat: not be able to create a car with the same license plate

--- src/modules/cars/useCases/car/createCar/createCarUseCase.spec.ts
+++ src/modules/cars/useCases/car/createCar/createCarUseCase.spec.ts
@@ -1,5 +1,6 @@
 import { CarRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarInMemory.repository";
-import { beforeEach, describe, it } from "vitest";
+import { AppError } from "@shared/errors/AppError";
+import { beforeEach, describe, expect, it } from "vitest";
 import { ICreateCarRequest } from "./createCar.types";
 import { CreateCarUseCase } from "./createCar.useCase";
 
@@ -25,5 +26,33 @@ describe('Create a car', () => {
     }
 
     await createCarUseCase.execute(newCar)
+  })
+
+  it('should not be able to create a car with a existing license plate', async () => {
+    await expect(async () => {
+      const car1: ICreateCarRequest = {
+        name: 'Teste name',
+        brand: 'Teste brand',
+        daily_rate: 1,
+        description: 'Teste description',
+        fine_amount: 10,
+        license_plate: 'asdasdasd',
+        category_id: 'teste'
+      }
+
+      await createCarUseCase.execute(car1)
+
+      const car2: ICreateCarRequest = {
+        name: 'Teste name',
+        brand: 'Teste brand',
+        daily_rate: 1,
+        description: 'Teste description',
+        fine_amount: 10,
+        license_plate: 'asdasdasd',
+        category_id: 'teste'
+      }
+
+      await createCarUseCase.execute(car2)
+    }).rejects.toBeInstanceOf(AppError)
   })
 })
