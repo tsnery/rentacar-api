443fd018323bff74b61f33dfae6e6f86863ff991
Tainan Nery <zaoldyeck@outlook.com>
2022-10-06 16:36:15 -0300

    feat: testing cars filter by brand

--- src/modules/cars/useCases/car/listCar/listCarsUseCase.spec.ts
+++ src/modules/cars/useCases/car/listCar/listCarsUseCase.spec.ts
@@ -56,4 +56,30 @@ describe('List availables cars', () => {
     expect(cars).length(1)
   })
 
+  it('should list all available cars by brand', async () => {
+    const newCar = await createCarUseCase.execute({
+      name: "Audi",
+      description: "Carro bonito",
+      daily_rate: 140.00,
+      license_plate: "ABC-1212",
+      fine_amount: 100,
+      brand: "Audi",
+      category_id: "fc064e81-85c5-49cb-ab6f-4a56e26faa04"
+    })
+
+    const newCar2 = await createCarUseCase.execute({
+      name: "Teste",
+      description: "Carro bonito",
+      daily_rate: 140.00,
+      license_plate: "ABC-2121",
+      fine_amount: 100,
+      brand: "Audi",
+      category_id: "fc064e81-85c5-49cb-ab6f-4a56e26faa04"
+    })
+
+    const cars = await listCarsUseCase.execute({ brand: 'Audi' })
+
+    expect(cars).length(2)
+  })
+
 })
