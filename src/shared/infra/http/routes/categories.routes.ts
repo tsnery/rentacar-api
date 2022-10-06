8fa03cd6399a8a711053a59d501c049f95b46dae
Tainan Nery <zaoldyeck@outlook.com>
2022-10-05 22:18:14 -0300

    feat: added authentication and admin check middlewares

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import multer from 'multer'
 import { CreateCategoryController } from '@modules/cars/useCases/category/createCategory/createCategory.controller'
 import { ImportCategoryController } from '@modules/cars/useCases/category/importCategory/importCategory.controller'
 import { ListCategoriesController } from '@modules/cars/useCases/category/listCategories/listCategories.controller'
+import { checkAuthentication } from '@shared/infra/middlewares/checkAuthentication/checkAuthentication.middleware'
+import { checkAdmin } from '@shared/infra/middlewares/checkAdmin'
 
 
 const categoriesRoutes = Router()
@@ -16,9 +18,19 @@ const upload = multer({
   dest: './tmp',
 })
 
-categoriesRoutes.post('/', createCategoryController.handle)
+categoriesRoutes.post(
+  '/',
+  checkAuthentication,
+  checkAdmin,
+  createCategoryController.handle
+)
 categoriesRoutes.get('/', listCategoriesContorller.handle)
-categoriesRoutes.post('/import', upload.single('file'), importCategoryController.handle)
+categoriesRoutes.post(
+  '/import', upload.single('file'),
+  checkAuthentication,
+  checkAdmin,
+  importCategoryController.handle
+)
 
 export {
   categoriesRoutes
--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -2,14 +2,24 @@ import { Router } from "express";
 
 import { CreateSpecificationController } from "@modules/cars/useCases/specification/createSpecification/createSpecification.controller";
 import { ListSpecificationsController } from "@modules/cars/useCases/specification/listSpecifications/listSpecifications.controller";
+import { checkAuthentication } from "@shared/infra/middlewares/checkAuthentication/checkAuthentication.middleware";
+import { checkAdmin } from "@shared/infra/middlewares/checkAdmin";
 
 const createSpecificationController = new CreateSpecificationController()
 const listSpecificationController = new ListSpecificationsController()
 
 const specificationsRoutes = Router()
 
-specificationsRoutes.post('/', createSpecificationController.handle)
-specificationsRoutes.get('/', listSpecificationController.handle)
+specificationsRoutes.post(
+  '/',
+  checkAuthentication,
+  checkAdmin,
+  createSpecificationController.handle
+)
+specificationsRoutes.get(
+  '/',
+  listSpecificationController.handle
+)
 
 export {
   specificationsRoutes
