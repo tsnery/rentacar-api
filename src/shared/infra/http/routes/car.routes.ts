587be0a02bbe8315c1b1f72df158c3df5a62c39e
Tainan Nery <zaoldyeck@outlook.com>
2022-10-05 22:11:54 -0300

    feat: added authentication and checkAdmin middlewares

--- src/shared/infra/http/routes/car.routes.ts
+++ src/shared/infra/http/routes/car.routes.ts
@@ -1,11 +1,18 @@
 import { CreateCarController } from "@modules/cars/useCases/car/createCar/createCar.controller";
+import { checkAdmin } from "@shared/infra/middlewares/checkAdmin";
+import { checkAuthentication } from "@shared/infra/middlewares/checkAuthentication/checkAuthentication.middleware";
 import { Router } from "express";
 
 const carsRoutes = Router()
 
 const createCarController = new CreateCarController()
 
-carsRoutes.post('/', createCarController.handle)
+carsRoutes.post(
+  '/',
+  checkAuthentication,
+  checkAdmin,
+  createCarController.handle
+)
 
 export {
   carsRoutes
