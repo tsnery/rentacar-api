d1a35c4becb7c4204265ab9d48c7e0d41de5208a
Tainan Nery <zaoldyeck@outlook.com>
2022-10-05 22:15:39 -0300

    fix: instace of error

--- src/shared/infra/middlewares/checkAdmin/index.ts
+++ src/shared/infra/middlewares/checkAdmin/index.ts
@@ -1,4 +1,5 @@
 import { UserRepository } from "@modules/accounts/infra/typeorm/repositories/user/user.repository";
+import { AppError } from "@shared/errors/AppError";
 import { NextFunction, Request, Response } from "express";
 
 export async function checkAdmin(request: Request, response: Response, next: NextFunction) {
@@ -9,7 +10,7 @@ export async function checkAdmin(request: Request, response: Response, next: Nex
   const user = await userRepository.findById(id)
 
   if (!user?.is_admin) {
-    throw new Error('User is not an admin!')
+    throw new AppError('User is not an admin!', 401)
   }
 
   return next()
