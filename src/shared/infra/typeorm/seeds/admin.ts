1d4472e4d282da54d5a398cf2d66307a0111ed2e
Tainan Nery <zaoldyeck@outlook.com>
2022-10-05 21:56:13 -0300

    feat: admin seed

---
+++ src/shared/infra/typeorm/seeds/admin.ts
@@ -0,0 +1,22 @@
+import { AppDataSource } from "../data-source"
+import { v4 as uuidV4 } from 'uuid'
+import { hash } from "bcryptjs"
+
+async function getConnection() {
+  return AppDataSource.initialize()
+}
+
+async function create() {
+  const connection = getConnection()
+
+  const id = uuidV4()
+  const password = await hash('admin', 8)
+
+  await (await connection).query(
+    `INSERT INTO USERS(id, name, email, driver_license, password, is_admin, created_at)
+    VALUES($1, $2, $3, $4, $5, $6, $7)`,
+    [id, 'Tainan Admin', 'admin@email.com', '123123', password, true, new Date()]
+  )
+}
+
+create().then(() => console.log('User admin created!'))
