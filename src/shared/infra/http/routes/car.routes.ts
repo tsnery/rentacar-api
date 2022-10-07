a43a5377dae5a32db1974689b96ee4ff72dca00c
Tainan Nery <zaoldyeck@outlook.com>
2022-10-06 21:53:18 -0300

    feat: listCars added to the routes

--- src/shared/infra/http/routes/car.routes.ts
+++ src/shared/infra/http/routes/car.routes.ts
@@ -1,4 +1,5 @@
 import { CreateCarController } from "@modules/cars/useCases/car/createCar/createCar.controller";
+import { ListCarsController } from "@modules/cars/useCases/car/listCars/listCars.controller";
 import { checkAdmin } from "@shared/infra/middlewares/checkAdmin";
 import { checkAuthentication } from "@shared/infra/middlewares/checkAuthentication/checkAuthentication.middleware";
 import { Router } from "express";
@@ -6,12 +7,18 @@ import { Router } from "express";
 const carsRoutes = Router()
 
 const createCarController = new CreateCarController()
+const listCarsController = new ListCarsController()
 
 carsRoutes.post(
   '/',
   checkAuthentication,
   checkAdmin,
   createCarController.handle
+)
+
+carsRoutes.get(
+  '/availables',
+  listCarsController.handle
 )
 
 export {
