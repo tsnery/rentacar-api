4b6851850c61dd71f8e0f2c7c679040f810054e1
Tainan Nery <zaoldyeck@outlook.com>
2022-10-10 15:06:23 -0300

    feat: route for create carsSpecificaitons

--- src/shared/infra/http/routes/car.routes.ts
+++ src/shared/infra/http/routes/car.routes.ts
@@ -1,5 +1,7 @@
 import { CreateCarController } from "@modules/cars/useCases/car/createCar/createCar.controller";
 import { ListCarsController } from "@modules/cars/useCases/car/listCars/listCars.controller";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/carSpecification/createCarSpecification/createCarSpecification.controller";
+
 import { checkAdmin } from "@shared/infra/middlewares/checkAdmin";
 import { checkAuthentication } from "@shared/infra/middlewares/checkAuthentication/checkAuthentication.middleware";
 import { Router } from "express";
@@ -8,6 +10,7 @@ const carsRoutes = Router()
 
 const createCarController = new CreateCarController()
 const listCarsController = new ListCarsController()
+const createCarSpecificationsController = new CreateCarSpecificationController()
 
 carsRoutes.post(
   '/',
@@ -20,6 +23,12 @@ carsRoutes.get(
   '/availables',
   listCarsController.handle
 )
+
+carsRoutes.post(
+  '/specifications/:id',
+  checkAuthentication,
+  checkAdmin,
+  createCarSpecificationsController.handle)
 
 export {
   carsRoutes
