d1a2cc9be5ac48077ef310929a81794e898c9675
Tainan Nery <zaoldyeck@outlook.com>
2022-10-10 15:06:04 -0300

    feat: added dependecy injection plus returning new car

--- src/modules/cars/useCases/carSpecification/createCarSpecification/createCarSpecification.useCase.ts
+++ src/modules/cars/useCases/carSpecification/createCarSpecification/createCarSpecification.useCase.ts
@@ -1,15 +1,21 @@
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarRepository } from "@modules/cars/repositories/ICarRepository";
 import { ISpecificationRepository } from "@modules/cars/repositories/ISpecificationRepository";
 import { AppError } from "@shared/errors/AppError";
+import { inject, injectable } from "tsyringe";
 import { ICreateCarSpecificationRequest } from "./createCarSpecification.types";
 
+@injectable()
 export class CreateCarSpecificationUseCase {
   constructor(
+    @inject('CarRepository')
     private carRepository: ICarRepository,
+
+    @inject('SpecificationRepository')
     private specificationRepository: ISpecificationRepository
   ) { }
 
-  async execute({ car_id, specifications_id }: ICreateCarSpecificationRequest): Promise<void> {
+  async execute({ car_id, specifications_id }: ICreateCarSpecificationRequest): Promise<Car> {
     const carExists = await this.carRepository.findById(car_id)
 
     if (!carExists) {
@@ -22,6 +28,6 @@ export class CreateCarSpecificationUseCase {
 
     await this.carRepository.create(carExists)
 
-    console.log('carExists', carExists)
+    return carExists
   }
 }
