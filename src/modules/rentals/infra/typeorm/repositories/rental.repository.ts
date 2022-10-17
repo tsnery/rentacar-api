aa24ce4ca50c392b59411ba83ce8448a793bb48a
Tainan Nery <zaoldyeck@outlook.com>
2022-10-17 18:51:13 -0300

    feat: rental repository along with typeorm

---
+++ src/modules/rentals/infra/typeorm/repositories/rental.repository.ts
@@ -0,0 +1,37 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRental.dto";
+import { IRentalRepository } from "@modules/rentals/repositories/IRentalRepository.types";
+import { AppDataSource } from "@shared/infra/typeorm/data-source";
+import { Repository } from "typeorm";
+import { Rental } from "../entities/Rental";
+
+export class RentalRepository implements IRentalRepository {
+
+  private repository: Repository<Rental>
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(Rental)
+  }
+
+  async findOpenRentalByCar(car_id: string): Promise<Rental | null> {
+    const openByCar = await this.repository.findOne({ where: { car_id } })
+
+    return openByCar
+  }
+
+  async findOpenRentalByUser(user_id: string): Promise<Rental | null> {
+    const openByUser = await this.repository.findOne({ where: { user_id } })
+
+    return openByUser
+  }
+  async create({ car_id, expected_return_date, user_id }: ICreateRentalDTO): Promise<Rental> {
+    const rental = this.repository.create({
+      car_id,
+      user_id,
+      expected_return_date,
+    })
+
+    await this.repository.save(rental)
+
+    return rental
+  }
+}
