fe09118c8c7868881b470c54d2a068767dbae78d
Tainan Nery <zaoldyeck@outlook.com>
2022-10-17 19:21:19 -0300

    chore: rental integration with typeorm

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,14 +1,33 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm"
 import { v4 as uuidV4 } from "uuid"
 
+@Entity('rentals')
 export class Rental {
+  @PrimaryColumn('uuid')
   id!: string
+
+  @Column('uuid')
   car_id!: string
+
+  @Column('uuid')
   user_id!: string
+
+  @Column('timestamp with time zone')
   start_date!: Date
+
+  @Column('timestamp with time zone')
   end_date!: Date
+
+  @Column('timestamp with time zone')
   expected_return_date!: Date
+
+  @Column('numeric')
   total!: number
+
+  @CreateDateColumn()
   created_at!: Date
+
+  @UpdateDateColumn()
   updated_at!: Date
 
   constructor() {
