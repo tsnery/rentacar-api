3fa50d93fcb4038b926346e0f5eaaf9f43a925da
Tainan Nery <zaoldyeck@outlook.com>
2022-10-20 19:01:49 -0300

    fix: importing files individually

--- src/shared/infra/typeorm/data-source.ts
+++ src/shared/infra/typeorm/data-source.ts
@@ -1,4 +1,20 @@
+import { User } from "@modules/accounts/infra/typeorm/entities/User";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { CarImage } from "@modules/cars/infra/typeorm/entities/CarImage";
+import { Category } from "@modules/cars/infra/typeorm/entities/Category";
+import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { DataSource } from "typeorm";
+import { CreateCategories1664201347336 } from "./migrations/1664201347336-CreateCategories";
+import { CreateSpecifications1664318699145 } from "./migrations/1664318699145-CreateSpecifications";
+import { CreateUsers1664372234367 } from "./migrations/1664372234367-CreateUsers";
+import { AlterUserDeleteUsername1664375932444 } from "./migrations/1664375932444-AlterUserDeleteUsername";
+import { AlterUserAddAvatar1664458126035 } from "./migrations/1664458126035-AlterUserAddAvatar";
+import { CreateCars1664836614638 } from "./migrations/1664836614638-CreateCars";
+import { CreateSpecificationsCars1665105117449 } from "./migrations/1665105117449-CreateSpecificationsCars";
+import { AlterSpecificationsCarsDropId1665424600449 } from "./migrations/1665424600449-AlterSpecificationsCarsDropId";
+import { CreateCarImages1665495589044 } from "./migrations/1665495589044-CreateCarImages";
+import { CreateRentals1665681935769 } from "./migrations/1665681935769-CreateRentals";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -8,7 +24,18 @@ export const AppDataSource = new DataSource({
   password: "rentacarpassword",
   database: "rentacar",
   logging: false,
-  entities: ["modules/**/infra/typeorm/entities/*.ts"],
-  migrations: ["shared/infra/typeorm/migrations/*.ts"],
+  entities: [User, Car, CarImage, Category, Specification, Rental],
+  migrations: [
+    CreateCategories1664201347336,
+    CreateSpecifications1664318699145,
+    CreateUsers1664372234367,
+    AlterUserDeleteUsername1664375932444,
+    AlterUserAddAvatar1664458126035,
+    CreateCars1664836614638,
+    CreateSpecificationsCars1665105117449,
+    AlterSpecificationsCarsDropId1665424600449,
+    CreateCarImages1665495589044,
+    CreateRentals1665681935769
+  ],
   subscribers: [],
 })
