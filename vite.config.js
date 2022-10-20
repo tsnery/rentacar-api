49fb091644c0f2451a54012aa7424fcf291179d1
Tainan Nery <zaoldyeck@outlook.com>
2022-10-20 01:51:13 -0300

    fix: path mapping

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
-    // ...
+    alias: {
+      "@modules": resolve(__dirname, "src", "modules"),
+      "@config": resolve(__dirname, "src", "config"),
+      "@shared": resolve(__dirname, "src", "shared"),
+      "@errors": resolve(__dirname, "src", "errors"),
+      "@middlewares": resolve(__dirname, "src", "middlewares"),
+      "@utils": resolve(__dirname, "src", "utils"),
+    },
   },
 });
