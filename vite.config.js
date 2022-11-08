9a29cad6d7f7311a16b05e2df13b0b63f046b942
Tainan Nery <zaoldyeck@outlook.com>
2022-11-07 23:51:54 -0300

    fix: vite custom paths

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "path";
 
 export default defineConfig({
-  test: {
-    alias: {
-      "@modules": resolve(__dirname, "src", "modules"),
-      "@config": resolve(__dirname, "src", "config"),
-      "@shared": resolve(__dirname, "src", "shared"),
-      "@errors": resolve(__dirname, "src", "errors"),
-      "@middlewares": resolve(__dirname, "src", "middlewares"),
-      "@utils": resolve(__dirname, "src", "utils"),
-    },
-  },
+  plugins: [tsconfigPaths()],
+  test: {},
 });
